b36600c1b50e54703a2996fb37699f84580a1237
Andrey S <nexinek@gmail.com>
2015-06-13 22:00:11 +0300

    Users resource, begining

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -10,15 +10,12 @@ describe "User pages" do
     it { should have_content('Sign up') }
     it { should have_title(full_title('Sign up')) }
   end
-end
 
-=begin
-RSpec.describe "UserPages", type: :request do
-  describe "GET /user_pages" do
-    it "works! (now write some real specs)" do
-      get user_pages_index_path
-      expect(response).to have_http_status(200)
-    end
+  describe "profile page" do
+    let(:user) { FactoryGirl.create(:user) }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
 end
-=end
